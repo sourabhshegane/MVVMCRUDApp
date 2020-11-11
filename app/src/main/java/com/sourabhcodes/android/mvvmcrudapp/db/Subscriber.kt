c2ee2ea34a359c69297c0064b8057bc7ce9280af
Sourabh Shegane <sgsourabh30@gmail.com>
2020-11-11 20:08:20 +0530

    Initial commit

---
+++ app/src/main/java/com/sourabhcodes/android/mvvmcrudapp/db/Subscriber.kt
@@ -0,0 +1,17 @@
+package com.sourabhcodes.android.mvvmcrudapp.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "subscriber")
+data class Subscriber(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "subscriber_id")
+    val id : Int,
+    @ColumnInfo(name = "subscriber_name")
+    val name: String,
+    @ColumnInfo(name = "subscriber_email")
+    val email: String
+) {
+}
