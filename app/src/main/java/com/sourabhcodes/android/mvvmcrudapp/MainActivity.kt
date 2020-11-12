79b199a86264a82a53de76b7802896dbcf00455c
Sourabh Shegane <sgsourabh30@gmail.com>
2020-11-12 11:01:20 +0530

    Initialized ViewModel Factory in MainActivity.kt and created method to get all the subscribers list

--- app/src/main/java/com/sourabhcodes/android/mvvmcrudapp/MainActivity.kt
+++ app/src/main/java/com/sourabhcodes/android/mvvmcrudapp/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.sourabhcodes.android.mvvmcrudapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.sourabhcodes.android.mvvmcrudapp.databinding.ActivityMainBinding
+import com.sourabhcodes.android.mvvmcrudapp.db.SubscriberDatabase
+import com.sourabhcodes.android.mvvmcrudapp.db.SubscriberRepository
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Observable {
+    private var TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var subscriberViewModel: SubscriberViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val dao = SubscriberDatabase.getInstance(application).subscriberDAO
+        val repository = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repository)
+
+        subscriberViewModel = ViewModelProvider(this, factory).get(SubscriberViewModel::class.java)
+        binding.subscriberViewModel = subscriberViewModel
+        binding.lifecycleOwner = this
+
+        displaySubscribersList()
+    }
+
+    private fun displaySubscribersList() {
+        subscriberViewModel.allSubscribers.observe(this, Observer {
+            Log.i(TAG, it.toString())
+        })
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
 
     }
 }
