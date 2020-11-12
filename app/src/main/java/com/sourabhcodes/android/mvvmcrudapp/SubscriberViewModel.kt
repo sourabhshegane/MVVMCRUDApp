d5d3d8ce484be2d523bfbf74af556fbf8568af3e
Sourabh Shegane <sgsourabh30@gmail.com>
2020-11-12 10:39:08 +0530

    Defined methods to perform CRUD

--- app/src/main/java/com/sourabhcodes/android/mvvmcrudapp/SubscriberViewModel.kt
+++ app/src/main/java/com/sourabhcodes/android/mvvmcrudapp/SubscriberViewModel.kt
@@ -3,9 +3,15 @@ package com.sourabhcodes.android.mvvmcrudapp
 import androidx.databinding.Bindable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sourabhcodes.android.mvvmcrudapp.db.Subscriber
 import com.sourabhcodes.android.mvvmcrudapp.db.SubscriberRepository
+import kotlinx.coroutines.launch
 
 class SubscriberViewModel(private val subscriberRepository: SubscriberRepository) : ViewModel() {
+
+    val allSubscribers = subscriberRepository.subscribers
+
     @Bindable
     val inputName = MutableLiveData<String>()
 
@@ -21,10 +27,31 @@ class SubscriberViewModel(private val subscriberRepository: SubscriberRepository
     }
 
     fun saveOrUpdate() {
+        val name = inputName.value!!
+        val email = inputEmail.value!!
 
+        insertSubscriber(Subscriber(0, name, email))
+        inputName.value = null
+        inputEmail.value = null
     }
 
     fun clearAllOrDelete() {
+        clearAllSubscribers()
+    }
 
+    fun insertSubscriber(subscriber: Subscriber) = viewModelScope.launch {
+        subscriberRepository.insertSubscriber(subscriber)
+    }
+
+    fun deleteSubscriber(subscriber: Subscriber) = viewModelScope.launch {
+        subscriberRepository.delete(subscriber)
+    }
+
+    fun updateSubscriber(subscriber: Subscriber) = viewModelScope.launch {
+        subscriberRepository.update(subscriber)
+    }
+
+    fun clearAllSubscribers() = viewModelScope.launch {
+        subscriberRepository.deleteAllSubscribers()
     }
 }
